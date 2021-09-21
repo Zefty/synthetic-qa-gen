f32f2f9a59c888e61a3f2f9c90e30a6e982bfe29
zefty <jaime.wu011@gmail.com>
2021-09-22 10:53:19 +1200

    update bash script

--- run_synthetic_qa_generator.sh
+++ run_synthetic_qa_generator.sh
@@ -1,2 +1,6 @@
 python SyntheticQAGenerator.py \
-    --csv_path 'splitted_covid_dump-covidQA.csv'
+    --csv_path 'splitted_covid_dump-covidQA.csv' \
+    --shard true \ 
+    --start_shard 0 \ 
+    --num_shards 20 \ 
+    --chunk_size 10
