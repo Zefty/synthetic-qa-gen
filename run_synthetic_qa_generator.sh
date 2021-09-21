ce58c25441fbd4d37dd6a842301a75c75b230ae7
zefty <jaime.wu011@gmail.com>
2021-09-22 10:59:57 +1200

    fixed spacing error in bash script

--- run_synthetic_qa_generator.sh
+++ run_synthetic_qa_generator.sh
@@ -1,6 +1,6 @@
 python SyntheticQAGenerator.py \
     --csv_path 'splitted_covid_dump-covidQA.csv' \
-    --shard true \ 
-    --start_shard 0 \ 
-    --num_shards 20 \ 
+    --shard true \
+    --start_shard 0 \
+    --num_shards 20 \
     --chunk_size 10
