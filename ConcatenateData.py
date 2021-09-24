9a2a939e3db253ad446ffda337ca273118c237ce
zefty <jaime.wu011@gmail.com>
2021-09-24 20:56:37 +1200

    update code

--- ConcatenateData.py
+++ ConcatenateData.py
@@ -6,9 +6,10 @@ from tqdm import tqdm
 
 directory = r'synthetic-qa-gen/data'
 for filename in tqdm(sorted(os.listdir(directory))):
+    print(filename)
     fileNameSplit = re.split('[_.]', filename)
     with open('{}/{}'.format(directory, filename), 'r') as infile:
-        with open('{}.{}'.format(fileNameSplit[0], fileNameSplit[-1]), 'a') as outfile:
+        with open('synthetic-qa-gen/{}.{}'.format(fileNameSplit[0], fileNameSplit[-1]), 'a') as outfile:
             outfile.write(infile.read())
             outfile.write("\n")
   
